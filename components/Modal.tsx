e7dbce3843562b4df5048f5d0e3f29e68b45e72a
Mohamed albashir <mohamedmaaax01@gmail.com>
2025-09-18 22:26:24 +0300

    p

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -10,9 +10,7 @@ const Modal: React.FC<ModalProps> = ({ onClose, children }) => {
     return (
         <div className="fixed inset-0 bg-black bg-opacity-50 z-50 flex justify-center items-center" onClick={onClose}>
             <div className="bg-white rounded-lg shadow-xl p-6 w-full max-w-7xl" onClick={(e) => e.stopPropagation()}>
-                <div className="flex justify-end">
-                    <button onClick={onClose} className="text-gray-500 hover:text-gray-700 text-2xl">&times;</button>
-                </div>
+
                 {children}
             </div>
         </div>
